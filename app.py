e9f54204ce853d983a7cacd4704ebe09caa2e035
ishangoel2005 <ishangoel2005@gmail.com>
2025-01-30 03:59:33 +0530

    Deployment

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
@@ -118,5 +117,4 @@ def predict_heart():
     return render_template('heart.html', prediction_text=result)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
